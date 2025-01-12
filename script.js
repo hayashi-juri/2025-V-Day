4b9a55ae36ec3e15f91853c0f33d496e80cbb8a3
hayashi-juri <hayashi.uoa.cn@gmail.com>
2025-01-12 21:05:32 +0900

    back button

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const image2 = document.querySelector(".image-2")
     const envelopeWrapper = document.querySelector(".envelope-wrapper");
     const heartElement = document.querySelector(".heart");
+    const backButton = document.querySelector(".back-button");
     const chocolateWrapper = document.querySelector(".chocolate-wrapper");
+    const gifButton = document.querySelector(".gif-button");
+    const backButtonChocolate = document.querySelector(".back-button-chocolate");
     let isOpened = false;
 
     // Yayボタンがクリックされたときに封筒を表示
@@ -86,6 +89,14 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     };
 
+    // 戻るボタンがクリックされたときの処理
+    const goBack = () => {
+        envelopeWrapper.classList.remove("show"); // 封筒を非表示
+        yayButton.style.display = "inline-block"; // Yayボタンを再表示
+        yeppiButton.style.display = "inline-block"; // Yeppiボタンを再表示
+        image1.style.display = "block"; // 猫の画像を再表示
+    };
+
     const showChocolate = () => {
         image1.style.display = "none";
         yeppiButton.style.display = "none"; // 非表示
@@ -94,10 +105,26 @@ document.addEventListener("DOMContentLoaded", () => {
         chocolateWrapper.classList.add("show");
     };
 
+    gifButton.addEventListener("click", () => {
+        alert("You clicked the gift!");
+        // 必要に応じて他のアクションをここに記述
+    });
+
+    const goBackFromChocolate = () => {
+        chocolateWrapper.classList.remove("show"); // Chocolate画面を非表示
+        yayButton.style.display = "inline-block"; // Yayボタンを再表示
+        yeppiButton.style.display = "inline-block"; // Yeppiボタンを再表示
+        image1.style.display = "block"; // 猫の画像を再表示
+    };
+
+
     // 封筒をクリックすると開閉
     envelopeWrapper.addEventListener("click", toggleEnvelope);
 
     // ボタンのイベントリスナーを設定
     yayButton.onclick = showEnvelope;
     yeppiButton.onclick = showChocolate;
+    backButton.addEventListener("click", goBack);
+    // 戻るボタンにイベントリスナーを追加
+    backButtonChocolate.addEventListener("click", goBackFromChocolate);
 });
